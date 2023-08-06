49e80c9b6e85b5c735f9e2b89e3d3763f62813c1
Colin <122695176+Zyransen@users.noreply.github.com>
2023-08-06 12:24:15 +0200

    add home and /employees routes

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
-import ListEmployeeComponent from './components/ListEmployeeComponent';
+import ListEmployeeComponent from './components/ListEmployeeComponent.jsx';
 
 function App() {
   return (
     <div>
-      <HeaderComponent/>
-      <div className='container'>
-        <ListEmployeeComponent />
-      </div>
-      <FooterComponent/>
+      <Router>
+        <HeaderComponent/>
+        <div class='container'>
+          <Routes>
+            <Route path='/' element={<ListEmployeeComponent/>}/>
+            <Route path='/employees' element={<ListEmployeeComponent/>}/>
+          </Routes>
+        </div>
+        <FooterComponent/>
+      </Router>
     </div>
     
   );
